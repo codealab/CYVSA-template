2bc25d7d21799b2136535a7e8495255424ea533f
tony15501 <your.marco.antonio.medina81@gmail.com>
2014-08-15 17:38:28 -0500

    creando partial view

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,16 +1,31 @@
 class ServicesController < ApplicationController
-  def index
-  end
-
-  def show
-  end
 
   def edit
+    @service = Service.find(params[:id])
+
   end
 
   def update
+
+    @service = Service.find(params[:id])
+    if @service.update_attributes(service_params)
+      flash[:success]= "Actualización exitosa"
+      redirect_to dashboard_path
+
+
+
+    else
+      render 'edit'
+
+    end
+
   end
 
-  def create
+  private
+  def service_params
+    params.require(:service).permit(:icon, :title, :content)
+
   end
+
+
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,6 @@ class StaticPagesController < ApplicationController
   def cyvsa_home
   	@homes = Home.all
     @projects = Project.order("id DESC")
+    @services = Service.all
 	end
 end
